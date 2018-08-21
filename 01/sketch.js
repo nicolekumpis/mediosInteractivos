0dc65951c1a2a7a382e0c1c5e7650e847803859f
nicolekumpis <42587826+nicolekumpis@users.noreply.github.com>
2018-08-21 14:52:38 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,84 @@
+function setup() {
+  createCanvas(600, 600);
+  background(239,234,216);
+  line(300,500,520,500)
+  line(300,0,420,580)
+  line(0,150,500,150)
+  line(200,80,200,520)
+  strokeWeight(6);
+  line(260,0,450,300)
+  strokeWeight(1);
+  line(100,180,250,100)
+  line(105,185,255,105)
+  line(110,190,260,110)
+  line(130,300,280,220)
+  line(135,305,285,225)
+  line(140,310,290,230)
+  line(280,300,450,300)
+  
+  fill(163,173,152)
+  noStroke()
+  ellipse(505,310,100,100)
+
+  
+  fill(139,128,160)
+  noStroke()
+  ellipse(360,300,80,80)
+  
+  fill(219,179,105)
+  noStroke()
+  triangle(450,150,500,300,380,280)
+ 
+  fill(136,68,57)
+  strokeWeight(2)
+  stroke(1)
+  quad(520,520,560,520,560,560,520,560)
+  
+  line(540,540,580,540)
+  line(540,545,580,545)
+  
+  fill(255,255,255)
+  stroke(171,193,192)
+  arc(0,150, 134, 134,PI,TWO_PI);
+  stroke(241,221,150)
+  arc(134,150,134,134,PI,TWO_PI)
+  stroke(182,104,48)
+  arc(268,150,134,134,PI,TWO_PI)
+  stroke(7,7,7)
+  arc(402,150,134,134,PI,TWO_PI)
+  noFill()
+  arc(60,220,134,134,PI,TWO_PI)
+  arc(194,220,134,134,PI,TWO_PI)
+  arc(250,300,134,134,PI,TWO_PI)
+  stroke(241,221,150)
+  arc(300,370,134,134,PI,TWO_PI)
+  
+  stroke(7,7,7)
+  strokeWeight(1)
+  line(0,300,60,300)
+  line(0,310,100,310)
+  
+  fill(136,68,57)
+  triangle(440,0,540,150,450,0)
+  triangle(460,0,540,110,470,0)
+  triangle(480,0,540,70,490,0)
+  
+  strokeWeight(4)
+  line(530,0,570,20)
+  line(100,0,0,40)
+  
+  fill(233,205,114)
+  noStroke()
+  quad(315,0,325,0,350,60,335,60)
+  
+  noFill()
+  strokeWeight(2)
+  stroke(1)
+  quad(270,0,280,0,70,160,60,150)
+  
+
+  
+}
+
+function draw() {
+}
